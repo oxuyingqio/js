ccaa0fba49a9e4a8cee76c39f6353e25f92d3195
oxuyingqio <oxuyingqio@qq.com>
2016-05-21 15:44:32 +0800

    X

--- src/main/resources/web/core/lang/Interface.js
+++ src/main/resources/web/core/lang/Interface.js
@@ -8,7 +8,7 @@
  * @param methods
  *            方法组
  */
-function Interface(name, methods) {
+var Interface = function(name, methods) {
 	// 判断构造参数个数
 	if (arguments["length"] != 2) {
 		throw "core.lang.Interface:构造参数异常.参数个数必须为2个,得到" + arguments["length"] + "个";
