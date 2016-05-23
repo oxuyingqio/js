897aea43ed94693ec680691bd0dde0b55c96941e
oxuyingqio <oxuyingqio@qq.com>
2016-05-23 09:29:21 +0800

    X

--- src/main/resources/web/core/lang/Class.js
+++ src/main/resources/web/core/lang/Class.js
@@ -7,6 +7,22 @@ var Class = function() {
 
 };
 
-Class.extend = function() {
-	alert(132)
+/**
+ * 类继承
+ * 
+ * @param subClass
+ * @param superClass
+ */
+Class.extend = function(subClass, superClass) {
+	var F = function() {
+	}
+
+	F.prototype = superClass.prototype;
+	subClass.prototype = new F();
+	subClass.prototype.constructor = subClass;
+
+	subClass.superClass = superClass.prototype;
+	if (superClass.prototype.constructor == Object.prototype.constructor) {
+		superClass.prototype.constructor = superClass;
+	}
 };
