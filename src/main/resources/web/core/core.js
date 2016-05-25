e07725b871e5cacc84e30110c71413caee7e528c
oxuyingqio <oxuyingqio@qq.com>
2016-05-25 15:42:42 +0800

    X

--- src/main/resources/web/core/core.js
+++ src/main/resources/web/core/core.js
@@ -5,4 +5,10 @@
  * 
  * 使用时,不允许存在名为"core"的全局变量
  */
-var core = {};
+(function() {
+	if (typeof (core) == "undefined") {
+		core = {};
+	} else {
+		throw "全局变量'core'被占用,请确保'core'未被占用后再进行使用";
+	}
+})();
