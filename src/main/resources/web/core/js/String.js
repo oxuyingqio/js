91d7617fa4f5376172e39d10f917b24d34cc2890
oxuyingqio <oxuyingqio@qq.com>
2016-05-27 15:54:43 +0800

    X

--- src/main/resources/web/core/js/String.js
+++ src/main/resources/web/core/js/String.js
@@ -18,3 +18,57 @@
 String.prototype.replaceAll = function(target, result) {
 	return this.replace(new RegExp(target, "g"), result);
 };
+
+/**
+ * 字符串转2进制
+ * 
+ * @param str
+ * @returns
+ */
+String.toBinaryString = function(str) {
+	// 返回的字符串
+	var rtnStr = [];
+
+	// 遍历每一个字符
+	for (var i = 0, length = str.length; i < length; i++) {
+		// 获取对应字符的2进制
+		var bs = str.charCodeAt(i).toString(2);
+
+		// 不足8位的补0
+		for (var j = bs.length; j < 8; j++) {
+			rtnStr.push(0);
+		}
+
+		rtnStr.push(bs);
+	}
+
+	// 返回
+	return rtnStr.join("");
+}
+
+/**
+ * 字符串转16进制
+ * 
+ * @param str
+ * @returns
+ */
+String.toHexString = function(str) {
+	// 返回的字符串
+	var rtnStr = [];
+
+	// 遍历每一个字符
+	for (var i = 0, length = str.length; i < length; i++) {
+		// 获取对应字符的16进制
+		var hs = str.charCodeAt(i).toString(16);
+
+		// 不足2位的补0
+		for (var j = hs.length; j < 2; j++) {
+			rtnStr.push(0);
+		}
+
+		rtnStr.push(hs);
+	}
+
+	// 返回
+	return rtnStr.join("");
+}
