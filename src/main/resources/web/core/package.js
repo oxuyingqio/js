f2c00b57e3e6d8082143ff6fa93a285f301bff62
oxuyingqio <oxuyingqio@qq.com>
2016-05-31 10:54:29 +0800

    X

--- src/main/resources/web/core/package.js
+++ src/main/resources/web/core/package.js
@@ -7,30 +7,30 @@
 		throw "全局变量'core'被占用,请确保'core'未被占用后再进行使用";
 	}
 
-	// core 核心包
+	// 核心包
 	core = {};
 
-	// lang 基础包
+	// 基础包
 	core.lang = {};
 
-	// constant 常量包
+	// 常量包
 	core.constant = {};
 
-	// util 工具包
+	// 工具包
 	core.util = {};
 
-	// event 事件包
+	// 事件包
 	core.event = {};
-	// event.window window事件包
+	// window事件包
 	core.event.window = {};
-	// event.document document事件包
+	// document事件包
 	core.event.document = {};
 
-	// log 日志包
+	// 日志包
 	core.log = {};
-	// log.output 日志输出包
+	// 日志输出包
 	core.log.output = {};
 
-	// example 示例包
+	// 示例包
 	core.example = {};
 })();
