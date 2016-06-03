354a79818320b473ae314180d3571f734e72ad7a
oxuyingqio <oxuyingqio@qq.com>
2016-06-03 15:06:03 +0800

    X

--- src/main/resources/web/core/html/element/model/Input.js
+++ src/main/resources/web/core/html/element/model/Input.js
@@ -6,4 +6,12 @@
  * 接口
  */
 
-core.html.element.model.Input = new core.lang.Interface("", []);
+/**
+ * @method show 展示元素
+ * @method hide 隐藏元素
+ * @method destroy 销毁元素
+ * @method convertHtml 转为HTML
+ * @method dealHtml 处理HTML
+ */
+core.html.element.model.Input = new core.lang.Interface("core.html.element.model.Input", [ "show", "hide", "destroy",
+		"convertHtml", "dealHtml" ]);
--- src/main/resources/web/core/html/element/viewer/input/Text.js
+++ src/main/resources/web/core/html/element/viewer/input/Text.js
@@ -8,4 +8,15 @@
 
 core.html.element.viewer.input.Text = (function() {
 
+	// 输入框
+	var input;
+
+	/**
+	 * 构造函数
+	 */
+	var Constructor = function() {
+
+	};
+	
+	
 })();
