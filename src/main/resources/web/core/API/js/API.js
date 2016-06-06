d854f35113efbe42d884e9456e50a3dec7aa4085
oxuyingqio <oxuyingqio@qq.com>
2016-06-06 11:58:33 +0800

    X

--- src/main/resources/web/core/API/js/API.js
+++ src/main/resources/web/core/API/js/API.js
@@ -4,3 +4,9 @@ function changePackage(selectPackage) {
 	$("#class").attr("src", "/~/core/API/" + url + "/class.html");
 	$("#desc").attr("src", "/~/core/API/" + url + "/desc.html");
 }
+
+function changeClass(selectClass) {
+	var url = selectClass.replaceAll("\\.", "/");
+
+	$("#desc").attr("src", "/~/core/API/" + url + "/desc.html");
+}
