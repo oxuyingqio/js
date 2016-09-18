416830e6cf852fefd9320f535600ffc0582c9467
oxuyingqio <oxuyingqio@qq.com>
2016-09-18 13:39:42 +0800

    X

--- src/main/resources/web/core/html/element/viewer/Input.js
+++ src/main/resources/web/core/html/element/viewer/Input.js
@@ -1,14 +1,14 @@
 /**
- * @name	Input
- * @package	core.html.element.viewer
- * @desc	输入控件
- * @type	类
+ * @name Input
+ * @package core.html.element.viewer
+ * @desc 输入控件
+ * @type 类
  * 
- * @constructor	core.html.element.viewer.Input(String id)
+ * @constructor core.html.element.viewer.Input(String id)
  * 
- * @extend	core.html.element.AbstractElement
+ * @extend core.html.element.AbstractElement
  * 
- * @date	2016年8月20日 11:56:33
+ * @date 2016年8月20日 11:56:33
  */
 
 core.html.element.viewer.Input = (function() {
@@ -23,6 +23,27 @@ core.html.element.viewer.Input = (function() {
 
 		// 调用父类构造
 		core.html.element.viewer.Input.superClass.constructor.call(this, id);
+
+		/**
+		 * name属性
+		 */
+		var name;
+
+		/**
+		 * 获取/设置name属性
+		 * 
+		 * @param name
+		 */
+		this.name = function() {
+
+			switch (arguments.length) {
+			case 0:
+				return name;
+			default:
+				name = arguments[0];
+				return this;
+			}
+		};
 	}
 	// 继承HTML元素公共抽象实现
 	core.lang.Class.extend(Constructor, core.html.element.AbstractElement);
@@ -40,6 +61,7 @@ core.html.element.viewer.Input = (function() {
 		// Input HTML
 		html.push("<input ");
 		html.push("id='" + this.id() + "' ");
+		html.push("name='" + this.name() + "' ");
 		// class
 		var clazz = this.clazz();
 		clazz === null || html.push("class='" + clazz + "' ");
